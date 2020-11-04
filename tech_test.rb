66f69011054a4781935bbe366084f96c82ab9a54
jarasmar <jarasmar@gmail.com>
2020-11-04 12:18:00 +0100

    Add age method

--- tech_test.rb
+++ tech_test.rb
@@ -10,7 +10,17 @@ class User
 
   # Returns and Integer representing the user's current age
   def age 
-    # code here
+    if Date.today.month > @date_of_birth.month
+      return Date.today.year - @date_of_birth.year
+    elsif Date.today.month < @date_of_birth.month
+      return Date.today.year - @date_of_birth.year - 1
+    elsif Date.today.month == @date_of_birth.month
+      if Date.today.day >= @date_of_birth.day
+        return Date.today.year - @date_of_birth.year
+      else
+        return Date.today.year - @date_of_birth.year - 1
+      end
+    end
   end
 
   # Returns a Date object for the user's next birthday
@@ -22,7 +32,8 @@ end
 tests = [
   Date.new(1986, 1, 1),
   Date.new(1988, Date.today.month, Date.today.day),
-  Date.new(1990, 12, 30)
+  Date.new(1992, 12, 30),
+  Date.new(1992, 06, 06)
 ]
 
 puts '===== ages ====='
